1eebb775879ba455a5f26ff05c6c8ed89e10b61e
Lucía Olivera <luliolifre@gmail.com>
2023-10-23 22:28:58 -0300

    ejercicios 1 y 2 hechos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,8 +6,58 @@ namespace CompAndDel
 {
     class Program
     {
+
+        //ejercicio 1
+        /*
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            IPipe pipeNull = new PipeNull();
+            picture = pipeNull.Send(picture);
+
+            IFilter filter1 = new FilterNegative();
+            IPipe pipeSerial = new PipeSerial(filter1, pipeNull);
+            picture = pipeSerial.Send(picture);
+
+            IFilter filter2 = new FilterGreyscale();
+            IPipe pipeSerial2 = new PipeSerial(filter2, pipeSerial);
+            picture = pipeSerial.Send(picture);
+
+            PictureProvider provider = new PictureProvider();
+            provider.SavePicture(picture, @"luke.jpg");
         }
+        */
+
+        //ejercicio 2
+        /*
+        static void Main(string[] args)
+        {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            IPipe pipeNull = new PipeNull();
+            picture = pipeNull.Send(picture);
+
+            IFilter filter1 = new FilterNegative();
+            IPipe pipeSerial = new PipeSerial(filter1, pipeNull);
+            picture = pipeSerial.Send(picture);
+
+            IFilter filter2 = new FilterGreyscale();
+            IPipe pipeSerial2 = new PipeSerial(filter2, pipeSerial);
+            picture = pipeSerial.Send(picture);
+
+            picture = pipeSerial2.Send(picture);
+
+            PictureProvider provider2 = new PictureProvider();
+            provider2.SavePicture(picture, "lukeModified.jpg");
+        }
+        */
+
+        
+
+
+
     }
 }
